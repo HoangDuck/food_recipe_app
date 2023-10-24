d11d5fff362ede36129dafdfbd37b31a7df93411
HoangDuck <hoanghuuduc22@gmail.com>
2023-10-24 08:23:05 +0700

    chore: commit generate file autoroute

---
+++ lib/routes/app_route.gr.dart
@@ -0,0 +1,127 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+part of 'app_route.dart';
+
+abstract class _$AppRoute extends RootStackRouter {
+  // ignore: unused_element
+  _$AppRoute({super.navigatorKey});
+
+  @override
+  final Map<String, PageFactory> pagesMap = {
+    CreateRecipeRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const CreateRecipeScreen(),
+      );
+    },
+    HomeRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const HomeScreen(),
+      );
+    },
+    OnBoardingRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const OnBoardingScreen(),
+      );
+    },
+    RecipeDetailsRoute.name: (routeData) {
+      final args = routeData.argsAs<RecipeDetailsRouteArgs>();
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: RecipeDetailsScreen(
+          key: args.key,
+          idMeal: args.idMeal,
+        ),
+      );
+    },
+  };
+}
+
+/// generated route for
+/// [CreateRecipeScreen]
+class CreateRecipeRoute extends PageRouteInfo<void> {
+  const CreateRecipeRoute({List<PageRouteInfo>? children})
+      : super(
+          CreateRecipeRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'CreateRecipeRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [HomeScreen]
+class HomeRoute extends PageRouteInfo<void> {
+  const HomeRoute({List<PageRouteInfo>? children})
+      : super(
+          HomeRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'HomeRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [OnBoardingScreen]
+class OnBoardingRoute extends PageRouteInfo<void> {
+  const OnBoardingRoute({List<PageRouteInfo>? children})
+      : super(
+          OnBoardingRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'OnBoardingRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [RecipeDetailsScreen]
+class RecipeDetailsRoute extends PageRouteInfo<RecipeDetailsRouteArgs> {
+  RecipeDetailsRoute({
+    Key? key,
+    required String idMeal,
+    List<PageRouteInfo>? children,
+  }) : super(
+          RecipeDetailsRoute.name,
+          args: RecipeDetailsRouteArgs(
+            key: key,
+            idMeal: idMeal,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'RecipeDetailsRoute';
+
+  static const PageInfo<RecipeDetailsRouteArgs> page =
+      PageInfo<RecipeDetailsRouteArgs>(name);
+}
+
+class RecipeDetailsRouteArgs {
+  const RecipeDetailsRouteArgs({
+    this.key,
+    required this.idMeal,
+  });
+
+  final Key? key;
+
+  final String idMeal;
+
+  @override
+  String toString() {
+    return 'RecipeDetailsRouteArgs{key: $key, idMeal: $idMeal}';
+  }
+}
